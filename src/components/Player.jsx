340f215685712e2502743ef6f46596a0adf858cb
정준화 <wnsghk112@naver.com>
2024-06-18 16:58:42 +0900

    refs로만 화면에 값 넣기 -> ref는 component를 재 로딩 하지 않아서 사용 용도가 다르다 반드시 state를 사용하여야 한다.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,17 +1,17 @@
 import { useState, useRef } from 'react';
 
 export default function Player() {
-  const [ enteredPlayerName, setEnteredPlayerName] = useState(null);
+  // const [ enteredPlayerName, setEnteredPlayerName] = useState(null);
 
   const playerName = useRef();
 
   function handleClick () {
-    setEnteredPlayerName(playerName.current.value);
+    // setEnteredPlayerName(playerName.current.value);
     playerName.current.value = '';
   }
   return (
     <section id="player">
-      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
+      <h2>Welcome {playerName.current ? playerName.current.value : 'unknown entity'}</h2>
       <p>
         <input ref={playerName} type="text"/>
         <button onClick={handleClick}>Set Name</button>
