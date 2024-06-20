18ae04a4ee5fecb52a3a0fd58c09ca4c0c197e55
Jeongumgu <33191329+Jeongumgu@users.noreply.github.com>
2024-06-20 14:49:38 +0900

    Update ResultModal.jsx
    remaningTime 반영

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,11 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react';
 
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+const ResultModal = forwardRef(function ResultModal({ targetTime, remaningTime}, ref) {
   const dialog = useRef();
+
+  const userLost = remaningTime <= 0;
+
+  const formattedRemaningTime = (remaningTime / 1000).toFixed(2);
 
   useImperativeHandle(ref, () => {
     return {
@@ -13,12 +17,12 @@ const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref)
   
   return (
     <dialog ref={ref} className="result-modal">
-      <h2>You { result } </h2>
+      {userLost && <h2>You lost </h2>}
       <p>
         The target time was <strong>{ targetTime }</strong> seconds.
       </p>
       <p>
-        You stopped the timer with <strong>X</strong> seconds left.
+        You stopped the timer with <strong>{formattedRemaningTime}</strong> seconds left.
       </p>
       <form method="dialog">
         <button>close</button>
