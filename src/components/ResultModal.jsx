baac6b5e55774dcf3c498535068bc8d8dd3bbcfc
정준화 <wnsghk112@naver.com>
2024-06-23 22:38:23 +0900

    createPortal 추가

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
 
 const ResultModal = forwardRef(function ResultModal({ result, targetTime, remaningTime, onReset}, ref) {
   const dialog = useRef();
